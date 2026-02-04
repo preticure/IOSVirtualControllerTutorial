481e068561ab9136cf3e33452c0d7dd184d13ef9
preticure <preticure112771@gmail.com>
2026-02-04 19:30:22 +0900

    GCVirtualControllerの警告修正

--- IOSVirtualControllerTutorial/App.swift
+++ IOSVirtualControllerTutorial/App.swift
@@ -5,6 +5,7 @@
 //  Created by preticure on 2026/02/04.
 //
 
+import GameController
 import SwiftUI
 import UIKit
 
@@ -17,13 +18,8 @@ class App: UIResponder, UIApplicationDelegate {
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
-
-        // Create the SwiftUI view that provides the window contents.
-        let contentView = ContentView()
-
-        // Use a UIHostingController as window root view controller.
         let window = UIWindow(frame: UIScreen.main.bounds)
-        window.rootViewController = UIHostingController(rootView: contentView)
+        window.rootViewController = GameViewController()
         self.window = window
         window.makeKeyAndVisible()
         return true
---
+++ IOSVirtualControllerTutorial/GameViewController.swift
@@ -0,0 +1,32 @@
+//
+//  GameContainerViewController.swift
+//  IOSVirtualControllerTutorial
+//
+//  Created by preticure on 2026/02/04.
+//
+
+import GameController
+import SwiftUI
+import UIKit
+
+class GameViewController: GCEventViewController {
+    private var hostingController: UIHostingController<ContentView>!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        hostingController = UIHostingController(rootView: ContentView())
+
+        addChild(hostingController)
+        view.addSubview(hostingController.view)
+        hostingController.didMove(toParent: self)
+
+        hostingController.view.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            hostingController.view.topAnchor.constraint(equalTo: view.topAnchor),
+            hostingController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            hostingController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            hostingController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        ])
+    }
+}
